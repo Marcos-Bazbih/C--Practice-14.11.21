10c9a8e03902955d03d7a0229c05944106e69300
Marcos Bazbih <marcosbaza100@gmail.com>
2021-11-14 23:40:24 +0200

    first commit

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,291 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //********__1__*********
+            //Console.WriteLine("hello there");
+            //********__1__*********
+
+
+            //********__2__*********
+            //Console.WriteLine("Marcos");
+            //********__2__*********
+
+
+            //********__3__*********
+            //string myName = Console.ReadLine();
+            //Console.WriteLine(myName);
+            //********__3__*********
+
+
+            //********__4__*********
+            //Console.WriteLine(5);
+            //********__4__*********
+
+
+            //********__5__*********
+            //int userNum = int.Parse(Console.ReadLine());
+            //Console.WriteLine(userNum);
+            //********__5__*********
+
+
+            //********__6__*********
+            //int userNum = int.Parse(Console.ReadLine());
+            //if (userNum > 25)
+            //{
+            //    Console.WriteLine("HI");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("BYE");
+            //}
+            //********__6__*********
+
+
+            //********__7__*********
+            //int userNum = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(userNum);
+            //}
+            //********__7__*********
+
+
+            //********__8__*********
+            //int userNum = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < userNum; i++)
+            //{
+            //    Console.WriteLine(userNum);
+            //}
+            //********__8__*********
+
+
+            //********__9__*********
+            //int userNum = int.Parse(Console.ReadLine());
+            //string userName = Console.ReadLine();
+            //for (int i = 0; i < userNum; i++)
+            //{
+            //    Console.WriteLine(userName);
+            //}
+            //********__9__*********
+
+
+            //********__10__*********
+            //int num1 = 2;
+            //int num2 = 4;
+            //int num3 = 6;
+            //int num4 = 8;
+            //int avg = (num1 + num2 + num3 + num4)/4;
+            //int sum = num1 + num2 + num3 + num4;
+            //Console.WriteLine(avg);
+            //Console.WriteLine(sum);
+            //********__10__*********
+
+
+            //********__11__*********
+            //int width = 20;
+            //int height = 10;
+            //int total = width * height;
+            //Console.WriteLine(total);
+            //********__11__*********
+
+
+            //********__12__*********
+            //int num1 = 10;
+            //int num2 = 20;
+            //int num3 = num1;
+            //Console.WriteLine(num1);
+            //Console.WriteLine(num2);
+            //num1 = num2;
+            //num2 = num3;
+            //Console.WriteLine(num1);
+            //Console.WriteLine(num2);
+            //********__12__*********
+
+
+            //********__13__*********
+            //int num1 = int.Parse(Console.ReadLine());
+            //int num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num1);
+            //Console.WriteLine(num2);
+            //********__13__*********
+
+
+            //********__15__*********
+            //char a1 = 'a';
+            //char a2 = '1';
+            //Console.WriteLine(a1);
+            //Console.WriteLine(a2);
+            //********__15__*********
+
+
+            //********__16__*********
+            //bool a = true;
+            //Console.WriteLine(a);
+            //********__16__*********
+
+
+            //********__17__*********
+            //int num1 = int.Parse(Console.ReadLine());
+            //int num2 = int.Parse(Console.ReadLine());
+            //int num3 = int.Parse(Console.ReadLine());
+            //int mul = num1 * num2 * num3;
+            //Console.WriteLine(mul);
+            //********__17__*********
+
+
+            //********__18__*********
+            //int first = int.Parse(Console.ReadLine());
+            //int second = int.Parse(Console.ReadLine());
+            //int result = second/first;
+            //Console.WriteLine(result);
+            //********__18__*********
+
+
+            //********__19__*********
+            //int userNum = int.Parse(Console.ReadLine());
+            //userNum++;
+            //Console.WriteLine(userNum);
+            //********__19__*********
+
+
+            //********__20__*********
+            //int userNum = int.Parse(Console.ReadLine());
+            //userNum *= 3;
+            //Console.WriteLine(userNum);
+            //********__20__*********
+
+
+            //********__21__*********
+            //int radius = int.Parse(Console.ReadLine());
+            //double area = (radius * radius) * 3.14;
+            //Console.WriteLine(area);
+
+            //double space = (radius * 2) * 3.14;
+            //Console.WriteLine(space);
+            //********__21__*********
+
+
+            //********__22__*********
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //********__22__*********
+
+
+            //********__23__*********
+            //for (int i = 0; i <= 100; i=i+2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //********__23__*********
+
+
+            //********__24__*********
+            //for (int i = 0; i <= 100; i = i + 3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //********__24__*********
+
+
+            //********__25__*********
+            //int sum = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    int userNum = int.Parse(Console.ReadLine());
+            //    sum += userNum;
+            //}
+            //Console.WriteLine(sum);
+            //int avg = sum / 5;
+            //Console.WriteLine(avg);
+            //********__25__*********
+
+
+            //********__26__*********
+            //int num1 = int.Parse(Console.ReadLine());
+            //int num2 = int.Parse(Console.ReadLine());
+            //int num3 = int.Parse(Console.ReadLine());
+            //int num4 = int.Parse(Console.ReadLine());
+            //int num5 = int.Parse(Console.ReadLine());
+
+            //int[] numArray = new int[] { num1, num2, num3, num4, num5 };
+
+            //int max= numArray[0];
+            //int min = numArray[0];
+
+            //for (int i = 0; i < numArray.Length; i++)
+            //{
+            //    if(numArray[i] > max)
+            //    {
+            //        max = numArray[i];
+            //    }
+            //    if(numArray[i] < min)
+            //    {
+            //        min = numArray[i];
+            //    }
+            //}
+            //Console.WriteLine(max);
+            //Console.WriteLine(min);
+            //********__26__*********
+
+
+            //********__27__*********
+            //int userNum = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //while (userNum > 0)
+            //{
+            //    userNum = int.Parse(Console.ReadLine());
+            //    sum += userNum;
+            //}
+            //Console.WriteLine(sum);
+            //********__27__*********
+
+
+            //********__28__*********
+            //int userNum = int.Parse(Console.ReadLine());
+            //int module;
+            //int sum = 0;
+            //while (userNum > 0)
+            //{
+            //    module = userNum % 10;
+            //    sum += module;
+            //    userNum /= 10;
+            //}
+            //Console.WriteLine(sum);
+            //********__28__*********
+
+
+            //********__29__*********
+            Random random = new Random();
+            int randomNum = random.Next(5);
+            int userNum = int.Parse(Console.ReadLine());
+            int tries = 0;
+            while (userNum != randomNum)
+            {
+                if (userNum > randomNum)
+                {
+                    Console.WriteLine("Bigger");
+                }
+                else if(userNum < randomNum)
+                {
+                    Console.WriteLine("Smaller");
+                }
+                else
+                {
+                    randomNum = random.Next(100);
+                    userNum = int.Parse(Console.ReadLine());
+                }
+                    tries++;
+            }
+            Console.WriteLine(tries);
+            //********__29__*********
+
+        }
+    }
+}
